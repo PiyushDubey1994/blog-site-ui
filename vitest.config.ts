7e4ae845f5958321f071c404dcc8f8fb7ef1a818
PiyusDubey1994 <Er_dubeypiyush@outlook.com>
2024-11-15 16:19:28 +0530

    fix
    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,3 +19,16 @@ export default defineConfig({
     emptyOutDir: true,
   },
 });
+
+
+export default {
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    }
+};
